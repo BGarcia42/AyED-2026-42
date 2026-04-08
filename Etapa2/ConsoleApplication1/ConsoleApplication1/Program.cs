4c10e97f4a5de1794bb206c67c28b0fba3827aab
pelado gaming <inf3rex3+1@gmail.com>
2026-04-08 14:12:52 -0300

    hola

---
+++ Etapa2/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peso;
+            Console.WriteLine("Escribe tu peso: ");
+            peso = int.Parse(Console.ReadLine());
+
+            if (peso >= 77)
+            {
+                Console.WriteLine("puedes inscribirte al torneo");
+            }
+            else
+            { 
+                Console.WriteLine("no cumples con el peso minimo, no puedes inscribirte");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}

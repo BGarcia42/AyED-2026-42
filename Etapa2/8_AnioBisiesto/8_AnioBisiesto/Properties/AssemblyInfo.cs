013a625de401ab4fe149ba8975eaf49437af95ad
pelado gaming <inf3rex3@gmail.com>
2026-04-14 14:39:04 -0300

    tp 8

--- Etapa2/8_AnioBisiesto/8_AnioBisiesto/Properties/AssemblyInfo.cs
+++ Etapa2/8_AnioBisiesto/8_AnioBisiesto/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("8_AnioBisiesto")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("8_AnioBisiesto")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ca315331-a44c-4e21-9a94-f49134b43f46")]
+[assembly: Guid("86826bde-720c-495b-ba46-a6b3fb192e5e")]
 
 // Version information for an assembly consists of the following four values:
 //

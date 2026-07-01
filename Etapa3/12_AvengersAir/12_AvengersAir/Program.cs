85fef91cf2505ffc789f11c40e4953219841a625
pelado gaming <inf3rex3+1@gmail.com>
2026-07-01 17:00:22 -0300

    tp 12 y 13

---
+++ Etapa3/12_AvengersAir/12_AvengersAir/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_AvengersAir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] asientos = new string[80, 6];
+            int disponible = 80;
+            int ocupado = 0;
+            int primera_clase = 0;
+            int economia = 0;
+            int emergencia = 0;
+            char opcion = '0';
+            while (opcion != 'X')
+            {
+                Console.WriteLine("-= MENÚ AVENGERSAIR =-");
+                Console.WriteLine("");
+                Console.WriteLine("1. Vender asiento");
+                Console.WriteLine("2. Devolver asiento");
+                Console.WriteLine("3. Modificar asiento");
+                Console.WriteLine("4. Calcular ventas");
+                Console.WriteLine("5. Buscar pasajeros por edad");
+                Console.WriteLine("6. Obtener asientos con DNI par");
+                Console.WriteLine("X. Salir");
+                Console.WriteLine("");
+                Console.WriteLine("Asientos disponibles: " + disponible);
+                Console.WriteLine("Asientos ocupados: " + ocupado);
+
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                opcion = char.ToUpper(input.KeyChar);
+
+                switch (opcion)
+                {
+                    case '1':
+                        Console.Clear();
+                        if (ocupado == 80) { Console.WriteLine("No hay asientos disponibles."); }
+                        else
+                        {
+                            Console.WriteLine("Asientos disponibles:");
+                            Console.WriteLine("");
+                            for (int i = 0; i < 80; i++)
+                            {
+                                if (asientos[i, 0] == null)
+                                {
+                                    Console.Write("Asiento " + (i + 1) + " - ");
+                                    if (i < 20)
+                                    {
+                                        Console.WriteLine("Primera clase");
+                                    }
+                                    else if (i + 1 > 39 && i + 1 < 44)
+                                    {
+                                        Console.WriteLine("Salida de emergencia");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Economía");
+                                    }
+                                }
+                            }
+                            Console.WriteLine("");
+                            Console.Write("Elegí un asiento: ");
+                            int n = int.Parse(Console.ReadLine());
+                            while (n > 80 || n < 1)
+                            {
+                                Console.Write("Número de asiento inválido. Intente de nuevo: ");
+                                n = int.Parse(Console.ReadLine());
+                            }
+                            if ((n - 1) < 20) { primera_clase += 200; }
+                            else if (n > 39 && n < 44) { emergencia += 80; }
+                            else { economia += 100; }
+                            Console.Write("Ingresá tu nombre: ");
+                            asientos[(n - 1), 0] = Console.ReadLine();
+                            Console.Write("Ingresá tu apellido: ");
+                            asientos[(n - 1), 1] = Console.ReadLine();
+                            Console.Write("Ingresá tu edad: ");
+                            asientos[(n - 1), 2] = Console.ReadLine();
+                            Console.Write("Ingresá tu número de DNI: ");
+                            asientos[(n - 1), 3] = Console.ReadLine();
+                            Console.Write("Ingresá tu nacionalidad: ");
+                            asientos[(n - 1), 4] = Console.ReadLine();
+                            Console.Write("Ingrese estado de ocupación: ");
+                            asientos[(n - 1), 5] = Console.ReadLine();
+                            Console.Clear();
+                            Console.WriteLine("Información cargada exitosamente.");
+                            disponible -= 1;
+                            ocupado += 1;
+                        }
+                        break;
+
+                    case '2':
+                        Console.Clear();
+                        Console.Write("Ingrese el asiento que desea devolver: ");
+                        int asiento = int.Parse(Console.ReadLine());
+                        if (asientos[(asiento - 1), 1] == null)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("El asiento " + asiento + " no está ocupado.");
+                        }
+                        else
+                        {
+                            for (int i = 0; i < 6; i++) { asientos[(asiento - 1), i] = null; }
+                            Console.Clear();
+                            Console.WriteLine("El asiento ha sido devuelto exitosamente.");
+                            ocupado -= 1;
+                            disponible += 1;
+                        }
+                        break;
+
+                    case '3':
+                        Console.Clear();
+                        if (ocupado == 0) { Console.WriteLine("No hay pasajeros para modificar."); }
+                        else
+                        {
+                            Console.WriteLine("Pasajeros:");
+                            Console.WriteLine("");
+                            for (int i = 0; i < 80; i++)
+                            {
+                                if (asientos[i, 0] != null)
+                                {
+                                    Console.Write("Asiento " + (i + 1) + " - ");
+                                    Console.Write("Nombre: " + asientos[i, 0] + " " + asientos[i, 1] + "; ");
+                                    Console.Write("Edad: " + asientos[i, 2] + "; ");
+                                    Console.Write("DNI: " + asientos[i, 3] + "; ");
+                                    Console.Write("Nacionalidad: " + asientos[i, 4] + "; ");
+                                    Console.WriteLine("Estado de ocupación: " + asientos[i, 5]);
+                                }
+                            }
+                            Console.WriteLine("");
+                            Console.Write("Elegí el asiento del pasajero a quien le vas a modificar la información: ");
+                            int pasajero = int.Parse(Console.ReadLine());
+                            while (asientos[(pasajero - 1), 1] == null)
+                            {
+                                Console.Write("Opción incorrecta. Elegí de nuevo: ");
+                                pasajero = int.Parse(Console.ReadLine());
+                            }
+                            Console.Write("Ingrese el nombre del pasajero: ");
+                            asientos[(pasajero - 1), 0] = Console.ReadLine();
+                            Console.Write("Ingrese el apellido del pasajero: ");
+                            asientos[(pasajero - 1), 1] = Console.ReadLine();
+                            Console.Write("Ingrese la edad del pasajero: ");
+                            asientos[(pasajero - 1), 2] = Console.ReadLine();
+                            Console.Write("Ingrese el DNI del pasajero: ");
+                            asientos[(pasajero - 1), 3] = Console.ReadLine();
+                            Console.Write("Ingrese la nacionalidad del pasajero: ");
+                            asientos[(pasajero - 1), 4] = Console.ReadLine();
+                            Console.Write("Ingrese el estado de ocupación del pasajero: ");
+                            asientos[(pasajero - 1), 5] = Console.ReadLine();
+                            Console.Clear();
+                            Console.Write("Información actualizada exitosamente.");
+                        }
+                        break;
+
+                    case '4':
+                        int total = primera_clase + emergencia + economia;
+                        Console.Clear();
+                        Console.WriteLine("El vuelo ha recaudado un total de $" + total + ".");
+                        break;
+
+                    case '5':
+                        Console.Clear();
+                        if (ocupado == 0) { Console.WriteLine("No hay pasajeros para mostrar."); }
+                        else
+                        {
+                            Console.Write("Ingresá la edad que querés buscar: ");
+                            string edad = Console.ReadLine();
+                            Console.Clear();
+                            Console.WriteLine("Pasajeros con " + edad + " años:");
+                            Console.WriteLine("");
+                            for (int i = 0; i < 80; i++)
+                            {
+                                if (asientos[i, 2] == edad) { Console.WriteLine(asientos[i, 1] + ", " + asientos[i, 0] + " - Asiento " + (i + 1)); }
+                            }
+                        }
+                        break;
+
+                    case '6':
+                        Console.Clear();
+                        if (ocupado == 0) { Console.WriteLine("No hay pasajeros para mostrar."); }
+                        else
+                        {
+                            Console.WriteLine("Pasajeros con DNI par:");
+                            Console.WriteLine("");
+                            for (int i = 0; i < 80; i++)
+                            {
+                                if (asientos[i, 3] != null)
+                                {
+                                    if (int.Parse(asientos[i, 3]) % 2 == 0) { Console.WriteLine(asientos[i, 1] + ", " + asientos[i, 0] + " - Asiento " + (i + 1)); }
+                                }
+                            }
+                        }
+                        break;
+
+                    case 'X':
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Opción inválida. Intente de nuevo.");
+                        break;
+                }
+                Console.WriteLine("");
+            }
+            Console.Clear();
+            Console.WriteLine("Gracias por utilizar el sistema de AvengersAir.");
+            Console.WriteLine("");
+            Console.WriteLine("Presione cualquier tecla para continuar.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa3/13_PERO/13_PERO/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_PERO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            char opcion;
+            int[,] misiones = new int[30, 5];
+            int registro = 0;
+            int proceso = 0;
+            int fila = 0;
+            int obj = 0;
+            int total_1 = 0;
+            int total_2 = 0;
+            int total_3 = 0;
+            int mision_1 = 0;
+            int mision_2 = 0;
+            int mision_3 = 0;
+
+            do
+            {
+                Console.WriteLine("==== MENÚ DEL P.E.R.O. ====");
+                Console.WriteLine("1. Registrar nueva misión");
+                Console.WriteLine("2. Ver todas las misiones");
+                Console.WriteLine("3. Cambiar estado de una misión");
+                Console.WriteLine("4. Listar misiones en curso");
+                Console.WriteLine("5. Misión con más objetos a extraer");
+                Console.WriteLine("6. Promedio de pegrilo por mapa");
+                Console.WriteLine("7. Filtrar por mapa");
+                Console.WriteLine("X. Salir");
+
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                opcion = char.ToUpper(input.KeyChar);
+                Console.Clear();
+
+                switch (opcion)
+                {
+                    case '1':
+                        if (registro > 29) { Console.WriteLine("¡Demasiadas misiones!"); }
+                        else
+                        {
+                            misiones[registro, 0] = registro + 1;
+                            Console.Write("Ingresá el mapa en el que ocurrirá la misión:  ");
+                            misiones[registro, 1] = int.Parse(Console.ReadLine());
+                            while (misiones[registro, 1] < 1 || misiones[registro, 1] > 3)
+                            {
+                                Console.Write("Ese mapa no es de este juegazo. Intená de nuevo:");
+                                misiones[registro, 1] = int.Parse(Console.ReadLine());
+                            }
+                            misiones[registro, 2] = rand.Next(1, 71);
+                            Console.Write("Ingresá el nivel de peligro de la misión:  ");
+                            misiones[registro, 3] = int.Parse(Console.ReadLine());
+                            while (misiones[registro, 3] < 1 || misiones[registro, 3] > 5)
+                            {
+                                Console.Write("Este nivel es demasiado PEGRILOSO... Intená de nuevo:");
+                                misiones[registro, 3] = int.Parse(Console.ReadLine());
+                            }
+                            misiones[registro, 4] = 0;
+                            registro += 1;
+                            Console.Clear();
+                            Console.WriteLine("¡La misión fue añadida con éxito!");
+                        }
+                        break;
+
+                    case '2':
+                        for (int i = 0; i < 30; i++)
+                        {
+                            if (misiones[i, 0] != 0)
+                            {
+                                Console.Write("Misión " + misiones[i, 0] + " - Mapa: ");
+                                if (misiones[i, 1] == 1) { Console.Write("Hagwarts"); }
+                                else if (misiones[i, 1] == 2) { Console.Write("La Casa del Viejo"); }
+                                else { Console.Write("El Laboratorio"); }
+                                Console.Write(" - Objetos a extraer: " + misiones[i, 2] + " - Nivel de peligro: " + misiones[i, 3] + " - Estado: ");
+                                if (misiones[i, 4] == 0) { Console.WriteLine("Pendiente"); }
+                                else if (misiones[i, 4] == 1) { Console.WriteLine("En curso"); }
+                                else { Console.WriteLine("Finalizada"); }
+                            }
+                        }
+                        break;
+
+                    case '3':
+                        Console.Write("Elegí la misión a la que queres cambiar el estado: ");
+                        int eleccion = int.Parse(Console.ReadLine());
+                        while (eleccion < 1 || eleccion > registro + 1)
+                        {
+                            Console.Write("No elegiste una misión existente. Intentá de nuevo: ");
+                            eleccion = int.Parse(Console.ReadLine());
+                        }
+                        if (misiones[eleccion - 1, 4] != 2)
+                        {
+                            misiones[eleccion - 1, 4] += 1;
+                            if (misiones[eleccion - 1, 4] == 1) { proceso += 1; }
+                            else if (misiones[eleccion - 1, 4] == 2) { proceso -= 1; }
+                            Console.Clear();
+                            Console.WriteLine("¡Se cambió el estado de la misión con éxito!");
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Esta misión ya está finalizada.");
+                        }
+                        break;
+
+                    case '4':
+                        if (proceso == 0) { Console.WriteLine("No hay misiones en proceso."); }
+                        else
+                        {
+                            Console.WriteLine("Misiones en curso:");
+                            Console.WriteLine("");
+                            for (int i = 0; i < 30; i++)
+                            {
+                                if (misiones[i, 4] == 1)
+                                {
+                                    Console.Write("Misión " + misiones[i, 0] + " - Mapa: ");
+                                    if (misiones[i, 1] == 1) { Console.Write("Hagwarts"); }
+                                    else if (misiones[i, 1] == 2) { Console.Write("La Casa del Viejo"); }
+                                    else { Console.Write("El Laboratorio"); }
+                                    Console.WriteLine(" - Objetos a extraer: " + misiones[i, 2] + " - Nivel de peligro: " + misiones[i, 3]);
+                                }
+                            }
+                        }
+                        break;
+
+                    case '5':
+                        for (int i = 0; i < 30; i++)
+                        {
+                            if (misiones[i, 2] > obj)
+                            {
+                                obj = misiones[i, 2];
+                                fila = i;
+                            }
+                        }
+                        Console.Write("La misión con más objetos a extraer es: ");
+                        Console.WriteLine("Misión " + misiones[fila, 0] + " - " + obj + " objetos a extraer");
+                        break;
+
+                    case '6':
+                        for (int i = 0; i < 30; i++)
+                        {
+                            if (misiones[i, 1] == 1) { total_1 += misiones[i, 3]; mision_1 += 1; }
+                            else if (misiones[i, 1] == 2) { total_2 += misiones[i, 3]; mision_2 += 1; }
+                            else { total_3 += misiones[i, 3]; mision_3 += 1; }
+                        }
+                        float promedio_1 = total_1 / mision_1;
+                        float promedio_2 = total_2 / mision_2;
+                        float promedio_3 = total_3 / mision_3;
+                        Console.WriteLine("Promedios de peligro según el mapa: ");
+                        Console.WriteLine("");
+                        Console.WriteLine("Hagwarts: " + promedio_1);
+                        Console.WriteLine("La Casa del Viejo: " + promedio_2);
+                        Console.WriteLine("El Laboratorio: " + promedio_3);
+                        break;
+
+                    case '7':
+                        Console.Write("Ingresá el número correspondiente al mapa que querés buscar: ");
+                        int mapa = int.Parse(Console.ReadLine());
+                        while (mapa < 1 || mapa > 3)
+                        {
+                            Console.Write("Ese mapa no es de este juegazo. Intená de nuevo:");
+                            mapa = int.Parse(Console.ReadLine());
+                        }
+                        Console.Clear();
+                        if (mapa == 1) { Console.WriteLine("Misiones en Hagwarts:"); }
+                        else if (mapa == 2) { Console.WriteLine("Misiones en La Casa del Viejo:"); }
+                        else { Console.WriteLine("Misiones en El Laboratorio:"); }
+                        for (int i = 0; i < 30; i++)
+                        {
+                            if (misiones[i, 1] == mapa)
+                            {
+                                Console.Write("Misión " + misiones[i, 0]);
+                                Console.Write(" - Objetos a extraer: " + misiones[i, 2] + " - Nivel de peligro: " + misiones[i, 3] + " - Estado: ");
+                                if (misiones[i, 4] == 0) { Console.WriteLine("Pendiente"); }
+                                else if (misiones[i, 4] == 1) { Console.WriteLine("En curso"); }
+                                else { Console.WriteLine("Finalizada"); }
+                            }
+                        }
+                        break;
+
+                    case 'X':
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción no válida. Intente de nuevo.");
+                        break;
+                }
+                Console.WriteLine("");
+            } while (opcion != 'X');
+            Console.Clear();
+            Console.WriteLine("Saliendo del sistema... ¡Esperemos que el PERO no sea letal!");
+            Console.WriteLine("Presione una tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
